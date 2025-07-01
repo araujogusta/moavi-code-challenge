13ce13c5b4c316b1590ab1df61099547630f1a87
Gustavo Oliveira <gusta01.oliveira@gmail.com>
2025-07-01 20:23:17 -0300

    feat: add MarkingImport and Marking models

--- pharmacy/core/models.py
+++ pharmacy/core/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class MarkingImport(models.Model):
+    id = models.AutoField(primary_key=True)
+    filename = models.CharField(max_length=255)
+    markings_count = models.IntegerField()
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "filename": self.filename,
+            "markings_count": self.markings_count,
+            "uploaded_at": self.uploaded_at,
+        }
+
+
+class Marking(models.Model):
+    id = models.AutoField(primary_key=True)
+    employee_id = models.IntegerField()
+    date = models.DateField()
+    hour = models.TimeField()
+
+    marking_import = models.ForeignKey(MarkingImport, on_delete=models.CASCADE)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "employee_id": self.employee_id,
+            "date": self.date,
+            "hour": self.hour,
+            "marking_import": self.marking_import.to_dict(),
+        }
